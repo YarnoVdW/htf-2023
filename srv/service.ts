e04589fdf1cee3c57add6698327da8eb2f6ecad6
YarnoVdW <yarno.vandeweyer@student.kdg.be>
2023-11-09 11:29:08 +0100

    Servicce

--- srv/service.ts
+++ srv/service.ts
@@ -3,7 +3,7 @@ import cds, { Service } from "@sap/cds/apis/services";
 const RangerService = async (srv: Service) => {
   srv.on("READ", "Pollutions", async (req) => {
     const data = req.data,
-      { starttime, endtime, room_ID } = data;
+      { ID, description,category_ID, criticality_code, contaminationPercentage, location_id } = data;
   });
 };
 
