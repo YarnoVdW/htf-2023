dae4b68a91cdeffc7140e4f9a206cf7b0e6a5c18
YarnoVdW <yarno.vandeweyer@student.kdg.be>
2023-11-09 11:30:09 +0100

    return data

--- srv/service.ts
+++ srv/service.ts
@@ -4,7 +4,9 @@ const RangerService = async (srv: Service) => {
   srv.on("READ", "Pollutions", async (req) => {
     const data = req.data,
       { ID, description,category_ID, criticality_code, contaminationPercentage, location_id } = data;
+      return data;
   });
+
 };
 
 export default {
